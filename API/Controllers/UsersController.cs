70657c7698a5b8fbe46475bf5381b9d190449282
Jose David Rodríguez Grijalba <josedavidrg14@hotmail.com>
2022-02-18 13:13:14 -0600

    End of section 16

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,8 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Claims;
 using System.Threading.Tasks;
-using API.Data;
 using API.DTOs;
 using API.Entities;
 using API.Extensions;
--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, JsonpClientBackend } from '@angular/common/http';
 import { Injectable, OnInit } from '@angular/core';
 import { ReplaySubject } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -42,6 +42,9 @@ export class AccountService {
 
 
   setCurrentUser(user:User){
+    user.roles = [];
+    const roles = this.getDecodedToken(user.token).role;
+    Array.isArray(roles)? user.roles = roles : user.roles.push(roles);
     localStorage.setItem('user', JSON.stringify(user));
     this.currentUserSource.next(user);
   }
@@ -49,6 +52,10 @@ export class AccountService {
   logout(){
     localStorage.removeItem('user');
     this.currentUserSource.next(null);
+  }
+
+  getDecodedToken(token:string){
+    return JSON.parse(atob(token.split('.')[1]));
   }
 
 }
